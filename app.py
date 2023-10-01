4a1f6ba51a2de88b8e3e232b3a37acea8a51fc06
Rayansh0071505 <98272246+Rayansh0071505@users.noreply.github.com>
2023-10-01 18:07:53 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,14 +1,35 @@
 import streamlit as st
 import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
 
 # Sample DataFrame
 data = {
-    'Name': ['Alice', 'Bob', 'Charlie'],
-    'Age': [25, 30, 35]
+    'x': np.random.rand(100),
+    'y': np.random.rand(100)
 }
 df = pd.DataFrame(data)
 
 # Streamlit app
-st.title('Pandas DataFrame Example')
-st.write('Here is a sample DataFrame:')
+st.title('Data Visualization with Pandas, NumPy, Matplotlib, and Seaborn')
+
+# Display the DataFrame
+st.write('Sample DataFrame:')
 st.write(df)
+
+# Scatter plot using Matplotlib
+st.write('### Scatter Plot using Matplotlib')
+plt.scatter(df['x'], df['y'])
+st.pyplot()
+
+# Histogram using Matplotlib
+st.write('### Histogram using Matplotlib')
+plt.hist(df['x'], bins=20)
+st.pyplot()
+
+# Heatmap using Seaborn
+st.write('### Heatmap using Seaborn')
+correlation_matrix = df.corr()
+sns.heatmap(correlation_matrix, annot=True)
+st.pyplot()
